b653a9d4b3d99c9af2fba0e4f02890b7a6f370cf
RoanRatering <114806917+Chistebueb@users.noreply.github.com>
2023-02-08 14:37:55 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,105 @@
+/*document.getElementById("input").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+            calculateWorkTime();
+    }
+  });
+*/
+
+function calculateWorkTime() {
+    let input = document.getElementById("input").value;
+    let timeStrings = input.split("/");
+    let workTime = 0;
+    for (let i = 0; i < timeStrings.length; i++) {
+      let time = timeStrings[i].split("-");
+      let start = time[0];
+      let end = time[1];
+      let breakIndex = end.indexOf("[");
+      let breakTime = 0;
+      if (breakIndex !== -1) {
+        let breakstring = end.substring(breakIndex + 1, end.indexOf("]"));
+        if(breakstring.includes("["))
+        {
+            breakstring = time[2].replace(']','');
+        }
+
+        breakTime = parseInt(breakstring);
+        end = end.substring(0, breakIndex);
+      }
+      let startHour = parseInt(start.substring(0, 2));
+      let startMinute = parseInt(start.substring(2, 4));
+      let endHour = parseInt(end.substring(0, 2));
+      let endMinute = parseInt(end.substring(2, 4));
+      let totalMinutes = (endHour - startHour) * 60 + (endMinute - startMinute) - breakTime;
+      workTime += totalMinutes;
+    }
+    let hours = Math.floor(workTime / 60);
+    let minutes = workTime % 60;
+    document.getElementById("result").innerHTML = "Total work time: " + hours + " hours and " + minutes + " minutes";
+    saveWorkTime();
+}
+
+
+
+function insertIntoInput(event) {
+    let workTime = event.target.innerHTML;
+    document.getElementById("input").value = workTime;
+  }
+  
+  function saveWorkTime() {
+    let workTime = document.getElementById("input").value;
+    let list = document.getElementById("saved-list");
+    let items = list.getElementsByTagName("li");
+    let duplicateIndex = -1;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].innerHTML === workTime) {
+        duplicateIndex = i;
+        break;
+      }
+    }
+    if (duplicateIndex !== -1) {
+      list.removeChild(items[duplicateIndex]);
+    }
+    if(items.length > 9)
+    {
+        list.removeChild(items[8]);
+    }
+    let newItem = document.createElement("li");
+    let newButton = document.createElement("button");
+    newButton.innerHTML = workTime;
+    newButton.addEventListener("click", insertIntoInput);
+    newItem.appendChild(newButton);
+    list.insertBefore(newItem, list.firstChild);
+    let savedWorkTimes = JSON.parse(localStorage.getItem("savedWorkTimes")) || [];
+    if (duplicateIndex !== -1) {
+      savedWorkTimes.splice(duplicateIndex, 1);
+    }
+    savedWorkTimes.unshift(workTime);
+    localStorage.setItem("savedWorkTimes", JSON.stringify(savedWorkTimes));
+  }
+  
+  window.onload = function() {
+    let savedWorkTimes = JSON.parse(localStorage.getItem("savedWorkTimes")) || [];
+    let list = document.getElementById("saved-list");
+    let uniqueWorkTimes = [];
+    for (let i = 0; i < savedWorkTimes.length; i++) {
+      if (!uniqueWorkTimes.includes(savedWorkTimes[i])) {
+        uniqueWorkTimes.push(savedWorkTimes[i]);
+        let newItem = document.createElement("li");
+        let newButton = document.createElement("button");
+        newButton.innerHTML = savedWorkTimes[i];
+        newButton.addEventListener("click", insertIntoInput);
+        newItem.appendChild(newButton);
+        list.insertBefore(newItem, list.firstChild);
+      }
+    }
+    localStorage.setItem("savedWorkTimes", JSON.stringify(uniqueWorkTimes));
+  }
+  
+
+  function clearWorkTimes() {
+    localStorage.removeItem("savedWorkTimes");
+    let list = document.getElementById("saved-list");
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+  }
